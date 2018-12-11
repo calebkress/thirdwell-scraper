d3616e15ab94337f9ea8dcfb40e2e4f72d236d2c
Caleb Kress <calebkress@gmail.com>
2018-12-11 15:55:23 -0600

    can grab full block of text from individual page

--- scraper.py
+++ scraper.py
@@ -1,1 +1,22 @@
-import urllib2
+import os
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url = 'https://www.thirdwell.org/Torah-Portion-Bereishit-Notes-Lesson-01.html'
+
+def getText(url):
+    title = url.split('/').pop()
+    if not os.path.exists('./text-files/' + title):
+        with open('./text-files/' + title, 'w'): pass
+
+    response = requests.get(url)
+    html = response.content
+
+    resultArr = []
+    soup = BeautifulSoup(html)
+    for p in soup.findAll('p'):
+        resultArr.append(p.text.replace('\n', ' ').replace('\\', ' ').replace('&#xa0;', ' '))
+
+    return ' '.join(resultArr)
+
+print getText(url)
